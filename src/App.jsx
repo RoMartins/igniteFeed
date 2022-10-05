74babb8fd4482795e13d271f7d2d0802d4b74683
RoMartins <rodrigomartins_santos1804@hotmail.com>
2022-10-05 02:17:45 -0300

    Component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { Header } from "./Components/Header"
 import {Post} from "./Post"
 import './global.css'
 
+import styles from './App.module.css'
+import { Sidebar } from "./Components/Sidebar"
+
 export function App() {
   return (
     <>
     <Header />
-    <Post 
-      author='Diego Fernades' 
-      content= 'lorem dkldamnafa akdnasjnasjk '   
-    />
-    <Post 
-      author='Rodrigo Martins' 
-      content= 'lorem dkldamnafa akdnasjnasjk '   
-    />
+    <div className={styles.wrapper}>
+
+      <Sidebar />
+      <main>
+        <Post author="rodrigo" content='afhjkfsjdkfks'/>
+        <Post author="joao" content='afhjkfsjdkfks'/>
+      </main>
+    </div>
     </>
     )
 
