0761ed972ac8780357bf459e449686696edb2bf8
RoMartins <rodrigomartins_santos1804@hotmail.com>
2022-10-07 01:33:40 -0300

    Component Avatar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css'
 import {PencilLine} from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 
 export function Sidebar() {
@@ -8,7 +9,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" alt="" srcset="" />
       
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/RoMartins.png" />
+        <Avatar src="https://github.com/RoMartins.png" />
         <strong>Rodrigo Martins</strong>
         <span>Web Developer</span>
       </div>   
